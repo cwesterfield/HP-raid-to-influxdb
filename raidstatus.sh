b44020d53e8bd1337af44d99739c6aaec00148fa
cwesterfield <colby.westerfield@gmail.com>
2017-01-09 11:10:52 -0600

    Final Edits

--- raidstatus.sh
+++ raidstatus.sh
@@ -2,6 +2,7 @@
 #Create table of output for Influxdb
 
 HPACUCLI=`which hpacucli`
+CURL==`which curl`
 HPACUCLI_RAW=/tmp/hpacucli.raw
 HPACUCLI_CLN=/tmp/hpacucli.log
 HPACUCLI_IMP=/tmp/influx.import
@@ -15,31 +16,35 @@ $HPACUCLI ctrl slot=0 pd all show status >> $HPACUCLI_RAW
 echo "@CONTROLLERINFO@" >> $HPACUCLI_RAW
 $HPACUCLI ctrl all show status >> $HPACUCLI_RAW
 
+##Insert Date for testing
+#echo $TIME
+
 ##Clean up spacing
 awk 'NF' $HPACUCLI_RAW | sed -e 's/^[ \t]*//' | grep -v 410i > $HPACUCLI_CLN
+
+##Empty old import file
+rm $HPACUCLI_IMP
 
 ##Split Flat file into chunks
 #Logical Drives
 
 for i in $HPACUCLI_CLN
-do
-#  cat $i | grep logicaldrive | tr -d '()' | sed 's/\,/:/g' | awk -F  ": " '{print "logical,host=hp_380_g6,log_num="$1,",raid="$2,",status="$3}' | sed 's/ ,/,/g'
-#Test for quotes
-cat $i | grep logicaldrive | tr -d '()' | sed 's/\,/:/g' | awk -F  ": " '{print "logical,host=hp_380_g6,log_num=""'\''"$1,"'\''"",raid=""'\''"$2,"'\''"",status=""'\''"$3"'\''"}' | sed 's/ '\'',/'\'',/g'
-#  cat $i | grep logicaldrive | tr -d '()' | sed 's/\,/:/g' | awk -F  ": " '{print "logical,host=hp_380_g6,log_num="$1,",status="$3}' | sed 's/ ,/,/g'
-done
+ do
+  cat $i | grep logicaldrive | tr -d '()' | sed 's/\,/:/g' | awk -F  ": " '{print "logical,Host=hp_380_g6,log_num="$1," Raid=\""$2"\",Status=\""$3"\""}' | sed 's/ "/"/g; s/ /\\ /1; s/ /\\ /2; s/ /\\ /3; s/ /\\ /4' >> $HPACUCLI_IMP
+ done
 
 #Physical Drives
 for j in $HPACUCLI_CLN
-do
-  cat $j | grep physicaldrive | cut -c 37- | tr -d ')' | sed 's/\,/:/g' | awk -F  ": " '{print "physical,host=hp_380_g6,bay=""'\''"$1,"'\''"",size=""'\''"$2,"'\''"",status=""'\''"$3"'\''"}' | sed 's/ '\'',/'\'',/g'
-#  cat $j | grep physicaldrive | cut -c 37- | tr -d ')' | sed 's/\,/:/g' | awk -F  ": " '{print "physical,host=hp_380_g6,bay="$1,",status="$3}' | sed 's/ ,/,/g'
-done
+ do
+  cat $j | grep physicaldrive | cut -c 37- | tr -d ')' | sed 's/\,/:/g' | awk -F  ": " '{print "physical,Host=hp_380_g6,Bay="$1," Size=\""$2,"\",Status=\""$3,"\""}'| sed 's/ "/"/g; s/ /\\ /1' >> $HPACUCLI_IMP
+ done
 
 #Controller
-#cat $HPACUCLI_CLN | sed -e '1,/CONTROLLERINFO/d'
 for k in $HPACUCLI_CLN
-do
-  cat $k | sed -e '1,/CONTROLLERINFO/d' | awk -F  ": " '{print "controller,host=hp_380_g6,item=""'\''"$1,"'\''"",status=""'\''"$2"'\''"}' | sed 's/ '\'',/'\'',/g'
+ do
+  cat $k | sed -e '1,/CONTROLLERINFO/d' | awk -F  ": " '{print "controller,Host=hp_380_g6,Item="$1," Status=\""$2,"\""}' | sed 's/ "/"/g; s/ /\\ /1; s/ /\\ /2' >> $HPACUCLI_IMP
+ done
 
-done
+#IMPORT!
+
+curl -i -XPOST 'http://10.0.1.100:8086/write?db=hp' --data-binary @$HPACUCLI_IMP
